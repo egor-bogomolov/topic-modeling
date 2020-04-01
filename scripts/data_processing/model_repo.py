a80667b33eb47489ebb8a2c4b0295faf27b67ded
egor-bogomolov <msicar.ot.on.yas@gmail.com>
2020-04-01 13:52:05 +0300

    Dump data on disk

--- scripts/data_processing/model_repo.py
+++ scripts/data_processing/model_repo.py
@@ -1,5 +1,7 @@
 import os
+import pickle
 import re
+import time
 
 import numpy as np
 
@@ -28,10 +30,10 @@ class ModelRepo:
         self.repos_data_files = [
             data_folder / 'bow' / f
             for f in os.listdir(self.repos_data_folder)
-            if not f.endswith('.lzo') and not f.endswith('.lzo.index') and f != '.gitkeep'
+            if not f.endswith('.lzo') and not f.endswith('.lzo.index') and f != '.gitkeep' and not f.is_dir()
         ]
 
-        self.repo_word_counts = None
+        self.repo_word_counts_folder = data_folder / 'bow' / 'rwc'
 
         self.repos_names_file = data_folder / 'stats' / 'repos_names.txt'
         self.names = None
@@ -54,45 +56,54 @@ class ModelRepo:
                 result.append(RepoWordCount(repo_name, word, int(count)))
         return result
 
-    def __extract_repo_data(self) -> List[RepoWordCount]:
-        print('Extracting repo data')
-        if self.repo_word_counts is None:
-            results = []
-            with Parallel(self.n_jobs) as pool:
-                n_files = len(self.repos_data_files)
-                file_batch_size = 10
-                for i in range(0, n_files, file_batch_size):
-                    file_batch = self.repos_data_files[i:i + file_batch_size]
-                    print(f'Loading {len(file_batch)} files')
-                    file_data = [
-                        file.open('r').readlines()
-                        for file in tqdm(file_batch)
-                    ]
-                    total_len = sum(map(len, file_data))
-                    chunk_size = total_len // (self.n_jobs * n_files)
-                    print(f'Found {total_len} repos, chunk size = {chunk_size}')
-                    chunks = [
-                        repos[start:start + chunk_size]
-                        for repos in file_data
-                        for start in range(0, len(repos), chunk_size)
-                    ]
-                    print(f'Processing {len(chunks)} chunks')
+    def __extract_from_file_batch(self, file_batch) -> List[RepoWordCount]:
+        print(f'Loading {len(file_batch)} files')
+        file_data = [
+            file.open('r').readlines()
+            for file in tqdm(file_batch)
+        ]
+        total_len = sum(map(len, file_data))
+        chunk_size = total_len // (self.n_jobs * len(file_batch))
+        print(f'Found {total_len} repos, chunk size = {chunk_size}')
+        chunks = [
+            repos[start:start + chunk_size]
+            for repos in file_data
+            for start in range(0, len(repos), chunk_size)
+        ]
+        print(f'Processing {len(chunks)} chunks')
+        start_time = time.time()
+        with Parallel(self.n_jobs) as pool:
+            chunk_results = pool([
+                delayed(self.__process_repos_chunk)(chunk, self.__pattern_repo, self.__pattern_words)
+                for chunk in chunks
+            ])
+        end_time = time.time()
+        print(f'Extracted repo word counts in {end_time - start_time} sec')
+        results = []
+        for chunk_result in chunk_results:
+            results += chunk_result
+        return results
 
-                    chunk_results = pool([
-                        delayed(self.__process_repos_chunk)(chunk, self.__pattern_repo, self.__pattern_words)
-                        for chunk in chunks
-                    ])
+    def __extract_repo_data(self) -> Generator[List[RepoWordCount]]:
+        if not self.repo_word_counts_folder.exists():
+            self.repo_word_counts_folder.mkdir()
 
-                for chunk_result in chunk_results:
-                    results += chunk_result
+            print('Extracting repo data')
+            n_files = len(self.repos_data_files)
+            file_batch_size = 10
+            for ind, i in enumerate(range(0, n_files, file_batch_size)):
+                file_batch = self.repos_data_files[i:i + file_batch_size]
+                rwc_batch = self.__extract_from_file_batch(file_batch)
+                print("Dumping repo word counts")
+                pickle.dump(rwc_batch, (self.repo_word_counts_folder / f'batch-{ind}.pkl').open('wb'))
+            print('Repo word counts extracted')
 
-            self.repo_word_counts = results
-        print('Repo word counts extracted')
-        return self.repo_word_counts
+        for file in os.listdir(self.repo_word_counts_folder):
+            return pickle.load((self.repo_word_counts_folder / file).open('rb'))
 
     def repos_names(self) -> List[str]:
         if not self.repos_names_file.exists():
-            self.names = list(set(rwc.name for rwc in self.__extract_repo_data()))
+            self.names = list(set(rwc.name for rwc_list in self.__extract_repo_data() for rwc in rwc_list))
             self.repos_names_file.open('w').write('\n'.join(self.names))
         if self.names is None:
             self.names = [line.strip() for line in self.repos_names_file.open('r').readlines()]
@@ -109,20 +120,28 @@ class ModelRepo:
         n_repos = len(repos_rev_index)
         clusters = clustering_model.get_clusters()
         n_clusters = clustering_model.n_clusters()
-
-        repo_word_counts = self.__extract_repo_data()
-        print('Sorting repo word counts')
-        repo_word_counts.sort(key=lambda rwc: rwc.word)
-
         cluster_embeddings = np.zeros((n_repos, n_clusters), dtype=np.int32)
-        last_word = None
-        cur_cluster = -1
-        for rwc in repo_word_counts:
-            if rwc.word != last_word:
-                cur_cluster = clusters[token_rev_index[rwc.word]]
-                last_word = rwc.word
-            repo_index = repos_rev_index[rwc.name]
-            cluster_embeddings[repo_index][cur_cluster] += rwc.count
+
+        for i, repo_word_counts in enumerate(self.__extract_repo_data()):
+            print(f'Step {i}')
+            print('Sorting repo word counts')
+            start_time = time.time()
+            repo_word_counts.sort(key=lambda rwc: rwc.word)
+            end_time = time.time()
+            print(f'Sorted in {end_time - start_time} sec')
+
+            print(f'Aggregating information from sorted rwc list')
+            start_time = time.time()
+            last_word = None
+            cur_cluster = -1
+            for rwc in repo_word_counts:
+                if rwc.word != last_word:
+                    cur_cluster = clusters[token_rev_index[rwc.word]]
+                    last_word = rwc.word
+                repo_index = repos_rev_index[rwc.name]
+                cluster_embeddings[repo_index][cur_cluster] += rwc.count
+            end_time = time.time()
+            print(f'Aggregatedin {end_time - start_time} sec')
 
         return cluster_embeddings
 
